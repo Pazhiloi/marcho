8ee18b27b0ed81e7a4aabfbe45d36068e3c8bc9b
Pazhiloi <martinsen06@gmail.com>
2021-01-23 00:04:02 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,10 @@ $(function () {
     autoplaySpead: 200
   });
 
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
 });
